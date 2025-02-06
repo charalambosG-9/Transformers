b7be956f3aaa0b459fee2383144174b9ad29cb36
Charalambos Georgiades <CGeorgiades1@sheffield.ac.uk>
2025-02-07 00:27:52 +0200

    Added padding_idx to Embeddings class to handle padded cases

--- transformer_classes.py
+++ transformer_classes.py
@@ -6,22 +6,22 @@ import math
 
 # Embeddings class
 class Embeddings(nn.Module):
-    def __init__(self, vocab_size, embed_dim):
+    def __init__(self, vocab_size, d_model, padding_idx = 0):
         super().__init__()
-        self.embed = nn.Embedding(vocab_size, embed_dim)
+        self.embed = nn.Embedding(vocab_size, d_model, padding_idx = padding_idx)
 
     def forward(self, x):
         return self.embed(x)
 
 # Positional Encoding class
 class PositionalEncoding(nn.Module):
-    def __init__(self, embed_dim, max_len = 1000, dropout = 0.1):
+    def __init__(self, d_model, max_len = 1000, dropout = 0.1):
         super().__init__()
         self.dropout = nn.Dropout(p = dropout)
         
-        pe = torch.zeros(max_len, embed_dim)
+        pe = torch.zeros(max_len, d_model)
         position = torch.arange(0, max_len).unsqueeze(1)  
-        div_term = torch.exp(torch.arange(0, embed_dim, 2).float() * (-math.log(10000.0) / embed_dim))
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
         pe[:, 0::2] = torch.sin(position * div_term)    
         pe[:, 1::2] = torch.cos(position * div_term)  
         pe = pe.unsqueeze(0)
@@ -31,6 +31,6 @@ class PositionalEncoding(nn.Module):
         return self.dropout(x)
     
 class MultiHeadedAttention(nn.Module):
-    def __init__(self, embed_dim, num_heads):
+    def __init__(self, d_model, num_heads):
         super().__init__()
-        assert embed_dim % num_heads == 0  
+        assert d_model % num_heads == 0  
